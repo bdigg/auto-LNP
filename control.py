4de2f94689d44cd78bbdb16abade7548e438a347
bdiggines <122548319+bdiggines@users.noreply.github.com>
2024-02-06 10:00:50 +0000

    Add START, NEXT STOP functions

--- control.py
+++ control.py
@@ -1,5 +1,6 @@
 import pressure as press
 import time
+import db 
 
 pstate = False
 
@@ -9,6 +10,58 @@ def main_loop():
         print("main loop")
         time.sleep(1)
 
+db.create_connection()
+db.create_setups_table()
+db.create_experiments_table()
+
+def start_process():
+    print("Starting")
+    if db.is_experiments_table_empty() == True:
+        print("No queued experiments - start")
+    else:
+        next_exp = db.get_experiment_names_in_order()[0] #Check that this is getting creation order not alphabetical
+        next_exp_info = db.get_experiment_info(next_exp)
+        experiment_t = 10 
+        start_t = time.time()
+        while True:
+            # Check if the elapsed time match the time limit
+            if (time.time() - start_t) > experiment_t:
+                break
+            # Wait until desired period time
+            if pstate == True:
+                #Set pump pressure
+                press.set_flow1(1,next_exp_info[2])
+                press.set_flow1(2,next_exp_info[3])
+                press.set_flow1(3,next_exp_info[4])
+                press.set_flow1(4,next_exp_info[5])
+                #Collect Experiment Data
+            else:
+                print("Set flow",next_exp_info[2:6])
+                time.sleep(1)
+                #press.set_pressure()
+        next_process()
+
+def next_process():
+    #Save experiment data to another database
+    print("Next process")
+    if db.is_experiments_table_empty() == True:
+        print("No queued experiments - next")
+        stop_process()
+    else:
+        prev_exp_id = db.get_experiment_ids_in_order()[0]
+        print("Previous exp id:",prev_exp_id)
+        print(db.get_experiment_info_with_id(prev_exp_id))
+        prev_exp_setup_id = (db.get_experiment_info_with_id(prev_exp_id))[6]
+        db.copy_setup_and_experiment_to_record(prev_exp_setup_id,prev_exp_id)
+        print(db.get_records_info(prev_exp_id)) 
+        #Remove from queue db
+        db.delete_experiment_by_id(prev_exp_id)
+        #Call start for next experiment
+        start_process()
+
+
+def stop_process():
+    print("Stawwwwwp")
 
 def setup_initiate():
     print("Initialising Setup")
@@ -21,27 +74,3 @@ def setup_calib(calibsetting):
     if pstate == True:
         press.pressure_calib(calibsetting)
 
-
-def setup_ch1(Reagent,Conc,Vol):
-    print(Reagent,Conc,Vol)
-    
-def setup_ch2(Reagent,Conc,Vol):
-    print(Reagent,Conc,Vol)
-    
-def setup_ch3(Reagent,Conc,Vol):
-    print(Reagent,Conc,Vol)
-
-def setup_ch4(Reagent,Conc,Vol):
-    print(Reagent,Conc,Vol)
-
-def exp_ch1(Flow):
-    print(Flow)
-    
-def exp_ch2(Flow):
-    print(Flow)
-    
-def exp_ch3(Flow):
-    print(Flow)
-
-def exp_ch4(Flow):
-    print(Flow)
